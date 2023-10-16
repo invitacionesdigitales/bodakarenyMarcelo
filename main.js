904b0cbe622f074d9c8a378e7b3fed6c5519fa27
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2023-10-16 14:51:19 -0300

    4

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 
 let diaEvento = new Date('December 29, 2023 21:30:00').getTime();
+let boton = document.querySelector("#btn")
 
+boton.addEventListener("click", () => {
 
+    Swal.fire({
+      title: '<strong>VER LA UBICACION<u></u></strong>',
+      icon: 'success',
+      html:
+        'INGRESÁ' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3475.6102692999048!2d-66.84726422447027!3d-29.410955075248776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjnCsDI0JzM5LjQiUyA2NsKwNTAnNDAuOSJX!5e0!3m2!1ses!2sar!4v1697478484421!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        'Podes ampliar el mapa',
+        
+        
+    })
+  });
 
 // diaEvento = new Date().getTime() + 1_000 * 20;
 
